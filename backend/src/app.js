e394a898563109babf2638d1cabdd0facbd50c1b
Saifurrahman <saifur13233@gmail.com>
2026-01-25 18:53:31 +0530

    Add Socket.IO real-time signaling and chat logic

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,7 @@ import mongoose from "mongoose";
 import { connectToSocket } from "./controllers/socketManager.js";
 
 import cors from "cors";
+import userRoutes from "./routes/users.routes.js";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -20,6 +21,8 @@ app.set("port", process.env.PORT || 8000);
 app.use(cors());
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
+
+app.use("/api/v1/users", userRoutes);
 
 // app.get("/home", (req, res) => {
 //   return res.json({ hello: "World" });
